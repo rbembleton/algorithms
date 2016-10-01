25c7c149959ddf7cc8788b4b8ee674f09bcb9d03
RB Embleton <RBEmbleton@aol.com>
2016-10-01 15:35:34 -0400

    update show tree

--- binary_search_tree/bst_node.js
+++ binary_search_tree/bst_node.js
@@ -1,17 +1,17 @@
 const BSTNode = function (val) {
-  this.value = val;
+  this.val = val;
   this.left = null;
   this.right = null;
 };
 
 BSTNode.prototype.insertVal = function (val) {
-  if (val < this.value) {
+  if (val < this.val) {
     if (this.left) {
       this.left.insertVal(val);
     } else {
       this.left = new BSTNode(val);
     }
-  } else if (val > this.value) {
+  } else if (val > this.val) {
     if (this.right) {
       this.right.insertVal(val);
     } else {
@@ -22,58 +22,35 @@ BSTNode.prototype.insertVal = function (val) {
 
 BSTNode.prototype.show = function () {
   if (this.left) { this.left.show(); }
-  console.log(this.value);
+  console.log(this.val);
   if (this.right) { this.right.show(); }
 };
 
-BSTNode.prototype.showTree = function () {
-  let firstRow = "";
-  let secondRow = "";
-  let leftThird = "";
-  let rightThird = "";
-
-  if (this.left) {
-    const below = this.left.showTree();
-    for (var i = 0; i < below.length; i++) {
-      firstRow += " ";
-      secondRow += " ";
-    }
-    secondRow += "/";
-    leftThird = below;
+growTree = function (node) {
+  if (node.left || node.right) {
+    const left = node.left ? growTree(node.left) : undefined;
+    const right = node.right ? growTree(node.right) : undefined;
+    return [left, node.val, right];
+  } else {
+    return node.val;
   }
-  firstRow += this.value;
-
-  if (this.right) {
-    const below = this.right.showTree();
-    secondRow += "\\";
-    for (var i = 0; i < below.length; i++) {
-      firstRow += " ";
-      secondRow += " ";
-    }
-    rightThird = below;
-  }
-
-  let leftArr = leftThird.split('\n');
-  let rightArr = rightThird.split('\n');
-  let thirdRow = "";
-
-  leftArr.forEach((el, idx) => {
-    thirdRow += leftArr[idx] + "  " + rightArr[idx] + '\n';
-  });
-
-  return firstRow + "\n" + secondRow + "\n" + thirdRow + "\n";
 };
 
-BSTNode.prototype.growTree = function () {
+showRow = function (arr) {
+  console.log("  " + arr[1] + "  ");
+  console.log(showRow(arr[0]) + " " + showRow(arr[2]));
+}
 
+BSTNode.prototype.showTree = function () {
+  const thisTree = growTree(this);
 };
 
 
 
 const root = new BSTNode(5);
-root.insertVal(3);
+root.insertVal(2);
 root.insertVal(6);
-// root.insertVal(4);
+root.insertVal(3);
+root.insertVal(4);
 
-root.show();
-console.log(root.showTree());
+root.showTree();
